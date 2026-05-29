fe93b9cd93acf36a7b1a39d2dd56480c9a8cd0b2
Arjay Laurenz Aquino <aquinoarjay141@gmail.com>
2026-05-29 17:09:04 +0200

    fourth

--- snack7.test.js
+++ snack7.test.js
@@ -7,4 +7,7 @@ const posts = [
 
 test("La funzione findPostById restituisce il post corretto dato l’array di post e l’id", () => {
     expect(findPostById(posts, 2)).toEqual({ id: 2, title: "Il Trono di Spade", slug: "Boh" })
+    expect(findPostById(posts, 3)).toBe(null)
+    expect(() => findPostById(posts, "ciao")).toThrow('"ciao" non è un id')
+    expect(() => findPostById([50, 96], 2)).toThrow('Array non è nel formato corretto')
 })
